16936a78787c7fb89cd5d7d19ba996948bb55c02
鸢一折折纸 <wky809982944@126.com>
2019-06-25 19:34:57 +0800

    11.接口和参数配置

--- src/main/java/com/example/controller/UserController.java
+++ src/main/java/com/example/controller/UserController.java
@@ -1,20 +1,30 @@
 package com.example.controller;
 
-import com.example.model.Cat;
 import com.example.model.User;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiImplicitParam;
+import io.swagger.annotations.ApiImplicitParams;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.web.bind.annotation.*;
 
+@Api(tags = "用户相关的请求")
 @RestController
 @RequestMapping("/user")
 public class UserController {
+    @ApiOperation("获取用户信息")
+    @ApiImplicitParams(
+            {
+                    @ApiImplicitParam(name = "username", value = "用户名", dataType = "int", paramType = "header", defaultValue = "zhangsan", example = "lisi"),
+                    @ApiImplicitParam(name = "password", value = "用户名", dataType = "int", paramType = "header", defaultValue = "zhangsan", example = "lisi")
+            }
+    )
     @GetMapping
-    public String getUser(@RequestBody Cat cat) {
-
+    public String getUser(String username, String password) {
         return "李四";
     }
-
+@ApiOperation("添加用户")
     @PostMapping
-    public String addUser(@RequestHeader("token")String token, String username) {
+    public String addUser(@RequestHeader("token") String token, String username) {
         return username;
     }
 
